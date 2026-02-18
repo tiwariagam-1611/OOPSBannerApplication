fd4602e83ffb3b01ab1712bf3d8a364804090e16
tiwariagam-1611 <tiwariagam16112003@gmail.com>
2026-02-18 14:52:29 +0530

    Coded UC2 to print OOPS using star pattern

---
+++ OOPSBannerApp.java
@@ -0,0 +1,29 @@
+/**
+* OOPSBannerApp
+*
+* This class demonstrates a simple Java application that displays the Object
+* Oriented Programming System OOPS acronym to the console.
+*
+* @author Developer
+* @version 2.0
+*/
+
+
+
+public class OOPSBannerApp
+{
+	public static void main(String[] args)
+	{
+
+		System.out.println("   ***   " + " " + "   ***    " + "******    " + "   *****  ");
+		System.out.println(" **   ** " + " " + " **   **  " + "**    **  " + " **       ");
+		System.out.println("**     **" + " " + "**     ** " + "**     ** " + "**        ");
+		System.out.println("**     **" + " " + "**     ** " + "**    **  " + " **       ");
+		System.out.println("**     **" + " " + "**     ** " + "******    " + "   ***    ");
+		System.out.println("**     **" + " " + "**     ** " + "**        " + "      **  ");
+		System.out.println("**     **" + " " + "**     ** " + "**        " + "       ** ");
+		System.out.println(" **   ** " + " " + " **   **  " + "**        " + "      **  ");
+		System.out.println("   ***   " + " " + "   ***    " + "**        " + " *****    ");
+
+	}
+}

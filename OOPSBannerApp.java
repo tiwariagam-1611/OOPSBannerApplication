0282c911ad570baf169d663847d092882a37c2c3
tiwariagam-1611 <tiwariagam16112003@gmail.com>
2026-02-18 15:19:12 +0530

    Coded UC3 to Print OOPS using String Join

---
+++ OOPSBannerApp.java
@@ -0,0 +1,29 @@
+/**
+* OOPSBannerApp UC3
+*
+* This class demonstrates a simple Java application that displays the Object
+* Oriented Programming System OOPS acronym to the console.
+*
+* @author Developer
+* @version 3.0
+*/
+
+
+
+public class OOPSBannerApp
+{
+	public static void main(String[] args)
+	{
+
+		System.out.println(String.join(" ","    ***   " , " " , "   ***    " , "******    " , "   *****  "));
+		System.out.println(String.join(" ","  **   ** " , " " , " **   **  " , "**    **  " , " **       "));
+		System.out.println(String.join(" "," **     **" , " " , "**     ** " , "**     ** " , "**        "));
+		System.out.println(String.join(" "," **     **" , " " , "**     ** " , "**    **  " , " **       "));
+		System.out.println(String.join(" "," **     **" , " " , "**     ** " , "******    " , "   ***    "));
+		System.out.println(String.join(" "," **     **" , " " , "**     ** " , "**        " , "      **  "));
+		System.out.println(String.join(" "," **     **" , " " , "**     ** " , "**        " , "       ** "));
+		System.out.println(String.join(" ","  **   ** " , " " , " **   **  " , "**        " , "      **  "));
+		System.out.println(String.join(" ","    ***   " , " " , "   ***    " , "**        " , " *****    "));
+
+	}
+}
